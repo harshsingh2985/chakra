c1de2a0919be40a5d69ebc924aca8fa478ff24d4
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2023-02-26 20:58:09 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component }  from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Harsh SINGH</h1>
+
+    </div>
+  );
+}
+
+export default App;
